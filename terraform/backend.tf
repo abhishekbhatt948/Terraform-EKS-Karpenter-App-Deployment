e58c1799d1eca9d8d2901a4b957e9a95dc8b155c
Abhishekbhatt948 <Abhishekbhatt948@gmail.com>
2026-02-06 22:37:07 +0530

    chng in pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,8 +3,6 @@ terraform {
     bucket         = "eks-demo-terraform-state-047038669736"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
     encrypt        = true
-    lock           = false
   }
 }
