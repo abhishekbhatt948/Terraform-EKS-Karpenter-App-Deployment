3d2e72f10516073ea09f19d95d5524f8c00c67db
Abhishekbhatt948 <Abhishekbhatt948@gmail.com>
2026-02-10 20:11:26 +0530

    chg in nodeclass ami details

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,18 +2,19 @@ resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
-  tags = {
-  key = "karpenter.sh/discovery"
-  value = var.cluster_name 
-  }
-  
   vpc_config {
     subnet_ids              = var.private_subnet_ids
     endpoint_private_access = true
     endpoint_public_access  = true
   }
-  
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
+}
+
+resource "aws_ec2_tag" "karpenter_cluster_sg_discovery" {
+  resource_id = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+  key         = "karpenter.sh/discovery"
+  value       = var.cluster_name
 }
