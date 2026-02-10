99200328c056e609336c4af783bafd5617ea3181
Abhishekbhatt948 <Abhishekbhatt948@gmail.com>
2026-02-10 21:53:01 +0530

    chg in root related provider detials

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -44,18 +44,20 @@ provider "kubernetes" {
 }
 
 provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(
+      data.aws_eks_cluster.this.certificate_authority[0].data
+    )
+    token = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubectl" {
+  load_config_file       = false
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(
     data.aws_eks_cluster.this.certificate_authority[0].data
   )
   token = data.aws_eks_cluster_auth.this.token
-}
-
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  ca_certificate         = base64decode(
-    data.aws_eks_cluster.this.certificate_authority[0].data
-  )
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
 }
